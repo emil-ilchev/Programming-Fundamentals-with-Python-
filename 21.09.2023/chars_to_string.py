d3108189728a3213b72e4d293a2321a7fd941308
emil-ilchev <134971190+emil-ilchev@users.noreply.github.com>
2023-09-23 22:56:14 +0300

    update

--- 21.09.2023/chars_to_string.py
+++ 21.09.2023/chars_to_string.py
@@ -1,4 +1,6 @@
 first_character = input()
 second_character = input()
 third_character = input()
-print(first_character + second_character + third_character)
+print((first_character), end="")
+print((second_character), end="")
+print((third_character), end="")
