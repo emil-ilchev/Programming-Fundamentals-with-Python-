8ab3c00d59d0af69063b28bf1bacf0e47c5f8619
emil-ilchev <134971190+emil-ilchev@users.noreply.github.com>
2023-09-23 23:25:11 +0300

    update

---
+++ 21.09.2023/sum_of_chars.py
@@ -0,0 +1,6 @@
+numbers_of_lines = int(input())
+total_sum = 0
+for character in range(numbers_of_lines):
+    current_character = input()
+    total_sum += ord(current_character)
+print(f"The sum equals: {total_sum}")
