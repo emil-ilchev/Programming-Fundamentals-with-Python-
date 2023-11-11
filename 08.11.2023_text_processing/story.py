b7544a06bea63b05ef27aeff66efbedaade2ca54
emil-ilchev <134971190+emil-ilchev@users.noreply.github.com>
2023-11-11 18:44:07 +0200

    update

--- 08.11.2023_text_processing/story.py
+++ 08.11.2023_text_processing/story.py
@@ -1,42 +1,38 @@
+# word = ""
+# words = ""
+# while True:
+#     if word != "end":
+#         words += word + " "
+#         word = input("Please type in a word:")
+#     else:
+#         break
+# print(words)
+#
 
-word = ""
-words = ""
-while True:
-    if word != "end":
-        words += word + " "
-        word = input("Please type in a word:")
-    else:
-        break
-print(words)
+# word = ""
+# words = ""
+# while True:
+#     if word != "end":
+#         word = input("Please type in a word:")
+#         words += word + " "
+#     else:
+#         break
+# print(words)
+#
+#
+# word = ""
+# words = ""
+# while True:
+#     words += word + " "
+#     word = input()
+#     if word == "end":
+#         break
+# print(words)
 
-
-word = ""
-words = ""
-while True:
-    if word != "end":
-        word = input("Please type in a word:")
-        words += word + " "
-    else:
-        break
-print(words)
-
-
-word = ""
-words = ""
-while True:
-    words += word + " "
-    word = input()
-    if word == "end":
-        break
-print(words)
-
-
-words = ""
-while True:
-    word = input()
-    words += word + " "
-    if word == "end":
-        break
-print(words)
-
-
+# words = ""
+# while True:
+#     word = input()
+#     words += word + " "
+#     if word == "end":
+#         break
+# print(words)
