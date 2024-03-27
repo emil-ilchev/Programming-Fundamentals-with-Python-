371fa17449e2cd403d0b5c849ebaa8d4368e41c8
emil-ilchev <134971190+emil-ilchev@users.noreply.github.com>
2024-03-27 10:35:50 +0200

    update

--- 27.10.2023_Exercise_classes_and_objects/weapon.py
+++ 27.10.2023_Exercise_classes_and_objects/weapon.py
@@ -4,7 +4,15 @@ class Weapon:
         self.bullets = bullets
 
     def shoot(self):
-        return
+        if self.bullets > 0:
+            self.bullets -= 1
+            return "shooting..."
+        return  "no bullets left"
+
+
+    def __repr__(self):
+        return f"Remaining bullets: {self.bullets}"
+
 
 weapon = Weapon(5)
 print(weapon.shoot())
@@ -14,4 +22,4 @@ print(weapon.shoot())
 print(weapon.shoot())
 print(weapon.shoot())
 print(weapon.shoot())
- print(weapon)
+print(weapon)
