537ce5e850acd8d0c293dd74caf283139183d368
emil-ilchev <134971190+emil-ilchev@users.noreply.github.com>
2024-03-27 10:14:54 +0200

    update

---
+++ 27.10.2023_Exercise_classes_and_objects/weapon.py
@@ -0,0 +1,17 @@
+class Weapon:
+
+    def __init__(self, bullets: int):
+        self.bullets = bullets
+
+    def shoot(self):
+
+
+weapon = Weapon(5)
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon)
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon.shoot())
+ print(weapon)
