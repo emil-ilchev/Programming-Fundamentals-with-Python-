38a3a2ccad65302d1c0f5643a7726704f0802f81
emil-ilchev <134971190+emil-ilchev@users.noreply.github.com>
2024-03-27 22:44:26 +0200

    update

--- 27.10.2023_Exercise_classes_and_objects/town.py
+++ 27.10.2023_Exercise_classes_and_objects/town.py
@@ -16,6 +16,14 @@ class Town:
 
 
 town = Town("Sofia")
+town1 = Town("Kiova")
+town2 = Town("Oslo")
+town2.set_latitude("12° 41\' 44.04\" N")
+town2.set_longitude("35° 19\' 15.94\" E")
+town1.set_latitude("85° 41\' 88.04\" N")
+town1.set_longitude("65° 19\' 55.94\" E")
 town.set_latitude("42° 41\' 51.04\" N")
 town.set_longitude("23° 19\' 26.94\" E")
+print(town1)
 print(town)
+print(town2)
