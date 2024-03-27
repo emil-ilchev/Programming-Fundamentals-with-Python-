eaa9d26bfdc1b1f288ac582b08ebd971110d7e93
emil-ilchev <134971190+emil-ilchev@users.noreply.github.com>
2024-03-27 22:01:24 +0200

    update

---
+++ 27.10.2023_Exercise_classes_and_objects/town.py
@@ -0,0 +1,15 @@
+class Town:
+
+    def __init__(self, name: str):
+        self.name = name
+        self.latitude - "0°N" =
+        self.longitude - "0°E" =
+
+    def set_latitude(self, latitude):
+        pass
+
+    def set_longitude(self, longitude):
+        pass
+
+    def __repr__(self):
+        pass
