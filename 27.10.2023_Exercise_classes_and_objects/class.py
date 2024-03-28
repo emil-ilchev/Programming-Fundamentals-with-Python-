aae8ad77063833cb20c952c9e037af2817c650d5
emil-ilchev <134971190+emil-ilchev@users.noreply.github.com>
2024-03-28 11:52:52 +0200

    update

--- 27.10.2023_Exercise_classes_and_objects/class.py
+++ 27.10.2023_Exercise_classes_and_objects/class.py
@@ -1,19 +1,24 @@
 class Class:
-    students = []
-    grades = []
+    __students_count = 22
 
     def __init__(self, name):
         self.name = name
-        self.__students_count = 22
+        self.students = []
+        self.grades = []
 
     def add_student(self, name: str, grade: float):
-        pass
+        if Class.__students_count > len(self.students):
+            self.students.append(name)
+            self.grades.append(grade)
 
-    def get_average_grade():
-        pass
+    def get_average_grade(self) -> str:
+        average_grade = sum(self.grades) / len(self.students)
+        return float(f"{average_grade:.2f}")
 
     def __repr__(self):
-        return f"The students in {class_name}: {students}. Average grade: {average_grade}"
+        students = ", ".join(self.students)
+        average_grade = self.get_average_grade()
+        return f"The students in {self.name}: {students}. Average grade: {average_grade}"
 
 
 a_class = Class("11B")
@@ -21,3 +26,4 @@ a_class.add_student("Peter", 4.80)
 a_class.add_student("George", 6.00)
 a_class.add_student("Amy", 3.50)
 print(a_class)
+# print(isinstance(a_class.get_average_grade(), float))
