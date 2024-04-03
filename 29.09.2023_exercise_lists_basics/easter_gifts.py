1832bfcbd4a8899a169931c8a8af1040756ea562
emil-ilchev <134971190+emil-ilchev@users.noreply.github.com>
2024-04-03 10:20:11 +0300

    update

---
+++ 29.09.2023_exercise_lists_basics/easter_gifts.py
@@ -0,0 +1,9 @@
+name_of_gifts = input().split()
+
+while True:
+    command = input()
+    command.pop(0)
+    # if command == "OutOfStock":
+    #     pass
+    print(command)
+    command = input()
---
+++ 29.09.2023_exercise_lists_basics/multiples_list.py
@@ -0,0 +1,28 @@
+factor = int(input())
+count = int(input())
+result = []
+for num in range(1, count + 1):
+    result.append(num * factor)
+print(result)
+
+
+
+# factor = int(input())
+# count = int(input())
+# result = []
+# current_number = factor
+# for num in range(1, count + 1):
+#     result.append(current_number)
+#     current_number += factor
+# print(result)
+
+
+# factor = int(input())
+# count = int(input())
+# result = []
+# for num in range(factor, factor * count + 1, factor):
+#     result.append(num)
+# print(result)
+
+
+
