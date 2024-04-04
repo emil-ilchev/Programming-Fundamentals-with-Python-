026f89136c3d7d5a4ded421f98d2b4c4d80453ac
emil-ilchev <134971190+emil-ilchev@users.noreply.github.com>
2024-04-04 22:21:09 +0300

    update

---
+++ 29.09.2023_exercise_lists_basics/list_test.py
@@ -0,0 +1,67 @@
+# numbers = [1, 2, 2, 3, 2, 5, 4, 2, 6, 8, 2, 3]
+# while 2 in numbers:
+#     numbers.remove(2)
+# print(numbers)
+
+# my_list = [1, 2, 3, "a", " b", "c", "d", " e", "f"]
+# char = my_list.pop(-3)
+# print(char)
+# print(my_list)
+
+# my_list = ["a", " b", "c", "d", " e", "f"]
+# print(' '.join([x for x in my_list if x != "d"]))
+
+# my_list = [1, 2, 3, "a", " b", "c", "d", " e", "f"]
+# while len(my_list) < 20:
+#     my_list.append("a")
+# print(my_list)
+
+# my_list = [1, 2, 3, "a", " b", "c", "d", " e", "f"]
+# my_list.insert(5, "Pesho")
+# print(my_list)
+
+# my_list = [1, 2, 3, "b", "a", " b", "c", "a", "a", "a", "d", " e", "a", "f"]
+# char = my_list.index("a")
+# print(char)
+
+# my_list = [1, 2, 3, "b", "a", " b", "c", "a", "a", "a", "d", " e", "a", "f"]
+# repetition = my_list.count("a")
+# print(repetition)
+# while my_list.count("a") > 1:
+#     my_list.remove("a")
+# print(my_list)
+
+# my_list = [1, 2, 3, "b", "a", " b", "c", "a", "a", "a", "d", " e", "a", "f"]
+# indexes = []
+# for index, element in enumerate(my_list):
+#     if element == "a":
+#         indexes.append(index)
+# print(indexes)
+
+# my_list = [1, 2, 3, "b", "a", " b", "c", "a", "a", "a", "d", " e", "a", "f"]
+# my_list.reverse()
+# print(my_list)
+# print(my_list[::-1])
+
+# my_list = [1, 2, 3, "b", "a", " b", "c", "a", "a", "a", "d", " e", "a", "f"]
+# del my_list[3]
+# print(my_list)
+
+# my_list = [1, 2, 3, "b", "a", " b", "c", "a", "a", "a", "d", " e", "a", "f"]
+# my_list.remove("a")
+# print(my_list)
+
+# my_list = [1, 2, 3, 4, 5]
+# my_second_list = my_list
+# my_second_list.remove(2)
+# print(my_list)
+# print(my_second_list)
+
+# my_list = [1, 2, 3, 4, 5]
+# my_second_list = my_list.copy()
+# my_second_list.remove(2)
+# print(my_list)
+# print(my_second_list)
+
+
+
