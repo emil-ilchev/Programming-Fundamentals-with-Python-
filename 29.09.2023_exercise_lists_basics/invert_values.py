ead2af662813704f895b2e947161773c8e323aaa
emil-ilchev <134971190+emil-ilchev@users.noreply.github.com>
2024-04-04 23:02:06 +0300

    update

--- 29.09.2023_exercise_lists_basics/invert_values.py
+++ 29.09.2023_exercise_lists_basics/invert_values.py
@@ -1,14 +1,14 @@
-input_as_string = input().split()
-opposite_list = []
-for element in input_as_string:
-    current_number = -int(element)
-    opposite_list.append(current_number)
-print(opposite_list)
+# input_as_string = input().split()
+# opposite_list = []
+# for element in input_as_string:
+#     current_number = -int(element)
+#     opposite_list.append(current_number)
+# print(opposite_list)
 
 
 
 
 
-# inpit_as_string = input()
-# numbers = [-int(num) for num in inpit_as_string.split()]
-# print(numbers)
+inpit_as_string = input()
+numbers = [-int(num) for num in inpit_as_string.split()]
+print(numbers)
---
+++ 29.09.2023_exercise_lists_basics/repeat_invert_values.py
@@ -0,0 +1,10 @@
+numbers = input().split()
+opposite_numbers = []
+for elements in numbers:
+    opposite_numbers.append(-int(elements))
+print(opposite_numbers)
+
+
+# numbers = input().split()
+# opposite_numbers = [-int(elements) for elements in numbers]
+# print(numbers)
