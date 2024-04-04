1242819cd14dbd7ffbd80c7b641eb5641195115b
emil-ilchev <134971190+emil-ilchev@users.noreply.github.com>
2024-04-04 20:12:07 +0300

    update

--- 29.09.2023_exercise_lists_basics/easter_gifts.py
+++ 29.09.2023_exercise_lists_basics/easter_gifts.py
@@ -1,10 +1,23 @@
 gifts = input().split()
 
 while True:
-    line = input()
-    if line == "No Money":
+    line = input().split()
+    if line[0] == "No":
         break
-    command_arg = line.split()
+    command_arg = line
     command = command_arg[0]
+
     if command == "OutOfStock":
-        pass
+        gift_name = command_arg[1]
+        for i in range(len(gifts)):
+            if gifts[i] == gift_name:
+                gifts[i] = "None"
+    if command == "Required":
+        gift_name = command_arg[1]
+        given_index = int(command_arg[2])
+        if 0 <= given_index < len(gifts):
+            gifts[given_index] = gift_name
+    if command == "JustInCase":
+        gift_name = command_arg[1]
+        gifts[-1] = gift_name
+print(' '.join([gift for gift in gifts if gift != 'None']))
