adb48f6817ee578af21812eed91688333e9819cf
emil-ilchev <134971190+emil-ilchev@users.noreply.github.com>
2024-04-05 12:07:13 +0300

    update

---
+++ 29.09.2023_exercise_lists_basics/repeat_multiples_list.py
@@ -0,0 +1,18 @@
+factor = int(input())
+count = int(input())
+result = []
+for el in range(1, count + 1):
+    result.append(el * factor)
+print(result)
+
+# factor = int(input())
+# count = int(input())
+# result = []
+# for el in range(factor, factor * count + 1, factor):
+#     result.append(el)
+# print(result)
+
+# factor = int(input())
+# count = int(input())
+# result = [el * factor for el in range(1, count + 1)]
+# print(result)
