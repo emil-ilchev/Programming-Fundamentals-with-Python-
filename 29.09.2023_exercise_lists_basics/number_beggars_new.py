85efa277eaa27823efb7a524b74e289cb6416af8
emil-ilchev <134971190+emil-ilchev@users.noreply.github.com>
2024-04-14 22:00:28 +0300

    update

---
+++ 29.09.2023_exercise_lists_basics/number_beggars_new.py
@@ -0,0 +1,11 @@
+string_of_integer = input().split(", ")
+count_of_beggars = int(input())
+sums = [0] * count_of_beggars
+numbers = []
+for num in string_of_integer:
+    numbers.append(int(num))
+for idx in range(len(numbers)):
+    beggar_idx = idx % count_of_beggars
+    sums[beggar_idx] += numbers[idx]
+print(sums)
+
