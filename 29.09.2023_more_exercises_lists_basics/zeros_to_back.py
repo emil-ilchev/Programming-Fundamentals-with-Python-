7ac4634e0e47d40847e394277671051e07be4ce7
emil-ilchev <134971190+emil-ilchev@users.noreply.github.com>
2024-04-15 21:57:27 +0300

    update

--- 29.09.2023_more_exercises_lists_basics/zeros_to_back.py
+++ 29.09.2023_more_exercises_lists_basics/zeros_to_back.py
@@ -7,3 +7,15 @@ while 0 in numbers:
 numbers.extend([0] * count)
 print(numbers)
 
+
+string_as_integer = input().split(", ")
+numbers = [int(num) for num in string_as_integer]
+count_of_zero = 0
+non_zero_numbers = []
+for num in numbers:
+    if num != 0:
+        non_zero_numbers.append(num)
+    else:
+        count_of_zero += 1
+non_zero_numbers.extend([0] * count_of_zero)
+print(non_zero_numbers)
