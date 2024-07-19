5f6579e7ce8c4dc6c3e2a4dad842683d8acac3a9
emil-ilchev <134971190+emil-ilchev@users.noreply.github.com>
2024-07-19 15:23:28 +0300

    update

--- 27.09.2023_lists_basics/numbers_filter.py
+++ 27.09.2023_lists_basics/numbers_filter.py
@@ -30,3 +30,33 @@ elif command == "even":
 
 
 
+# n = int(input())
+# list_of_numbers = []
+# final_list = []
+#
+# for i in range(n):
+#     numbers = int(input())
+#     list_of_numbers.append(numbers)
+#
+# command = input()
+#
+# for num in list_of_numbers:
+#     if command == "even":
+#         if num % 2 == 0:
+#             final_list.append(num)
+#     elif command == "odd":
+#         if num % 2 != 0:
+#             final_list.append(num)
+#     elif command == "negative":
+#         if num < 0:
+#             final_list.append(num)
+#     elif command == "positive":
+#         if num >= 0:
+#             final_list.append(num)
+#
+# print(final_list)
+
+
+
+
+
