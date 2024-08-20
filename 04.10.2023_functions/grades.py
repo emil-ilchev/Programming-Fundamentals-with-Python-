b5f1159ee17d7ca2ab306175a5806b7e3d6f2213
emil-ilchev <134971190+emil-ilchev@users.noreply.github.com>
2024-08-20 13:07:29 +0300

    update

--- 04.10.2023_functions/grades.py
+++ 04.10.2023_functions/grades.py
@@ -13,3 +13,21 @@ def grades():
 
 
 grades()
+
+# grades_data = float(input())
+#
+#
+# def grades():
+#     if 2.00 <= grades_data <= 2.99:
+#         return "Fail"
+#     elif 3.00 <= grades_data <= 3.49:
+#         return "Poor"
+#     elif 3.50 <= grades_data <= 4.49:
+#         return "Good"
+#     elif 4.50 <= grades_data <= 5.49:
+#         return "Very Good"
+#     elif 5.50 <= grades_data <= 6.00:
+#         return "Excellent"
+#
+#
+# print(grades())
