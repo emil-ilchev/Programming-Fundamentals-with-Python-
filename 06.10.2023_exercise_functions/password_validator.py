6f70d47b3e9d51acff5f919994c752d6f1db527d
emil-ilchev <134971190+emil-ilchev@users.noreply.github.com>
2024-08-27 09:23:49 +0300

    update

---
+++ 06.10.2023_exercise_functions/password_validator.py
@@ -0,0 +1,60 @@
+def password_validator():
+    password = input()
+    letters_count = 0
+    digits_count = 0
+    symbol_count = 0
+
+    if not 5 < len(password) < 11:
+        print("Password must be between 6 and 10 characters")
+    for el in password:
+        if el.isalpha():
+            letters_count += 1
+        elif el.isdigit():
+            digits_count += 1
+        else:
+            symbol_count += 1
+
+    if symbol_count > 0:
+        print("Password must consist only of letters and digits")
+    if digits_count < 2:
+        print("Password must have at least 2 digits")
+    if 5 < len(password) < 11 and digits_count > 1 and symbol_count == 0:
+        print("Password is valid")
+
+
+password_validator()
+
+# def password_validator():
+#     password = input()
+#     letters_list = []
+#     digits_list = []
+#     symbol_list = []
+#
+#     sum_of_characters = False
+#     letters_and_digits = False
+#     least_two_digits = False
+#     for el in password:
+#         if el.isalpha():
+#             letters_list.append(el)
+#         if el.isdigit():
+#             digits_list.append(el)
+#         if not el.isalpha() and not el.isdigit():
+#             symbol_list.append(el)
+#
+#     if 5 < len(password) < 11:
+#         sum_of_characters = True
+#     else:
+#         print("Password must be between 6 and 10 characters")
+#     if len(letters_list) > 0 and len(symbol_list) < 1:
+#         letters_and_digits = True
+#     else:
+#         print("Password must consist only of letters and digits")
+#     if len(digits_list) > 1:
+#         least_two_digits = True
+#     else:
+#         print("Password must have at least 2 digits")
+#     if sum_of_characters and letters_and_digits and least_two_digits:
+#         print("Password is valid")
+#
+#
+# password_validator()
