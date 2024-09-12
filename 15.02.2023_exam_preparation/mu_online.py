0f7bba323bc46d53b7f2cb7dd4c9a4df2215c91f
emil-ilchev <134971190+emil-ilchev@users.noreply.github.com>
2024-09-12 09:56:27 +0300

    update

---
+++ 15.02.2023_exam_preparation/mu_online.py
@@ -0,0 +1,31 @@
+rooms = input().split("|")
+MAX_HEALTH = 100
+health = 100
+bitcoins = 0
+
+for index,room in enumerate(rooms, start=1):
+    command, amount = room.split()
+    amount = int(amount)
+    if command == "potion":
+        if health + amount > MAX_HEALTH:
+            print(f"You healed for {MAX_HEALTH - health} hp.")
+            health = MAX_HEALTH
+        else:
+            health += amount
+            print(f"You healed for {amount} hp.")
+        print(f"Current health: {health} hp.")
+    elif command == "chest":
+        bitcoins += amount
+        print(f"You found {amount} bitcoins.")
+    else:
+        health -= amount
+        if health > 0:
+            print(f"You slayed {command}.")
+        else:
+            print(f"You died! Killed by {command}.")
+            print(f"Best room: {index}")
+            exit(0)
+
+print("You've made it!")
+print(f"Bitcoins: {bitcoins}")
+print(f"Health: {health}")
