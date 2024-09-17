922d1585a018583c33f398c95a443bea29824f21
emil-ilchev <134971190+emil-ilchev@users.noreply.github.com>
2024-09-17 11:17:47 +0300

    update

--- 11.10.2023_lists_advanced/even_numbers.py
+++ 11.10.2023_lists_advanced/even_numbers.py
@@ -1,16 +1,13 @@
+
 string_with_numbers = input().split(", ")
-new_list = []
-for index, el in enumerate(string_with_numbers):
-    if int(el) % 2 == 0:
-        new_list.append(index)
+new_list = [index for index, el in enumerate(string_with_numbers) if int(el) % 2 == 0]
 print(new_list)
 
 
 
-
-
-
-
 # string_with_numbers = input().split(", ")
-# new_list = [index for index, el in enumerate(string_with_numbers) if int(el) % 2 == 0]
+# new_list = []
+# for index, el in enumerate(string_with_numbers):
+#     if int(el) % 2 == 0:
+#         new_list.append(index)
 # print(new_list)
