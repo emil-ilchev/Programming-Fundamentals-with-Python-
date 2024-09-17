f382d9d624cf247724c77498ccad505a35b6ca5e
emil-ilchev <134971190+emil-ilchev@users.noreply.github.com>
2024-09-17 09:15:42 +0300

    update

--- 11.10.2023_lists_advanced/palindrome_strings.py
+++ 11.10.2023_lists_advanced/palindrome_strings.py
@@ -1,12 +1,20 @@
+def pilandrome():
+    # palindrome_list = input()
+    # palindrome = input()
+    result = []
+    number = 0
+    for word in palindrome_list.split():
+        if word == word[::-1]:
+            result.append(word)
+
+        if word == palindrome:
+            number += 1
+
+    print(result)
+    print(f"Found palindrome {number} times")
+
+
 palindrome_list = input()
 palindrome = input()
-result = []
-number = 0
-for word in palindrome_list.split():
-    if word == word[::-1]:
-        result.append(word)
 
-    if word == palindrome:
-        number += 1
-print(result)
-print(f"Found palindrome {number} times")
+pilandrome()
