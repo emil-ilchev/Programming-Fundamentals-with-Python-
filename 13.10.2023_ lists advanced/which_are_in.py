3fb63adbb5ea055dcfd1823874263ca9d578f037
emil-ilchev <134971190+emil-ilchev@users.noreply.github.com>
2024-09-20 10:06:07 +0300

    update

--- 13.10.2023_ lists advanced/which_are_in.py
+++ 13.10.2023_ lists advanced/which_are_in.py
@@ -9,3 +9,10 @@ for first_string in first_sequences:
             substring.append(first_string)
             break
 print(substring)
+
+
+# first_string = input().split(", ")
+# second_string = input().split(", ")
+#
+# new_list = [first_el for first_el in first_string if any(first_el in second_el for second_el in second_string)]
+# print(new_list)
