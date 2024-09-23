d44fa2018b7c6f6b06f7870f626042ec2bfddf88
emil-ilchev <134971190+emil-ilchev@users.noreply.github.com>
2024-09-23 12:12:14 +0300

    update

--- 13.10.2023_ lists advanced/numbers_clasiffication.py
+++ 13.10.2023_ lists advanced/numbers_clasiffication.py
@@ -13,3 +13,22 @@ print(f"Odd: {', '.join(map(str, odd))}")
 
 
 
+# def positives(num):
+#     return ', '.join([x for x in num if int(x) >= 0])
+#
+# def negatives(num):
+#     return ', '.join([x for x in num if int(x) < 0])
+#
+# def even(num):
+#     return ', '.join([x for x in num if int(x) % 2 == 0])
+#
+# def odd(num):
+#     return ', '.join([x for x in num if int(x) % 2 != 0])
+#
+#
+# numbers_at_str = input().split(", ")
+#
+# print(f"Positives: {positives(numbers_at_str)}")
+# print(f"Negative: {negatives(numbers_at_str)}")
+# print(f"Even: {even(numbers_at_str)}")
+# print(f"Odd: {odd(numbers_at_str)}")
